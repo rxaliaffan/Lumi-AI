dbcd66e85ed3d59e7b4c421c5d0639207523a270
rxaliaffan <ali.universityofkarachiwork@gmail.com>
2026-05-02 18:48:22 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
-import Sidebar from '@/components/Sidebar';
-import Header from '@/components/Header';
-import ScanCard from '@/components/ScanCard';
-import RiskScore from '@/components/RiskScore';
-import MedicinesTable from '@/components/MedicinesTable';
+import Sidebar from '../components/Sidebar';
+import Header from '../components/Header';
+import ScanCard from '../components/ScanCard';
+import RiskScore from '../components/RiskScore';
+import MedicinesTable from '../components/MedicinesTable';
 
 export default function HomePage() {
   return (
