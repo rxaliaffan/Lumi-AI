8fa257f3702e178e5d7e7e943565c0f3d44ba129
rxaliaffan <ali.universityofkarachiwork@gmail.com>
2026-05-02 18:57:19 +0500

    Add new components to HomePage layout

--- app/page.js
+++ app/page.js
@@ -4,25 +4,54 @@ import ScanCard from '../components/ScanCard';
 import RiskScore from '../components/RiskScore';
 import MedicinesTable from '../components/MedicinesTable';
 
+// NEW components (you must create these next)
+import SchedulePanel from '../components/SchedulePanel';
+import AdherenceCard from '../components/AdherenceCard';
+import PharmacyPanel from '../components/PharmacyPanel';
+import ActionButtons from '../components/ActionButtons';
+
 export default function HomePage() {
   return (
     <main className="flex min-h-screen bg-[#F8FAFC]">
+      
+      {/* Sidebar */}
       <Sidebar />
 
+      {/* Main Content */}
       <section className="flex-1 p-8 space-y-8">
+        
+        {/* Header */}
         <Header />
 
+        {/* Main Grid */}
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-          <div className="lg:col-span-2">
+          
+          {/* LEFT SIDE */}
+          <div className="lg:col-span-2 space-y-8">
+            
+            {/* Scan Card */}
             <ScanCard />
+
+            {/* Risk + Adherence */}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <RiskScore />
+              <AdherenceCard />
+            </div>
+
+            {/* Medicines Table */}
+            <MedicinesTable />
+
+            {/* Action Buttons */}
+            <ActionButtons />
           </div>
 
-          <div>
-            <RiskScore />
+          {/* RIGHT SIDE */}
+          <div className="space-y-6">
+            <SchedulePanel />
+            <PharmacyPanel />
           </div>
+
         </div>
-
-        <MedicinesTable />
       </section>
     </main>
   );
